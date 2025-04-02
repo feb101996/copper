1dd09ceee55f1fdd62c26ccbaa3728485906a9fe
feb101996 <jaudianclepher@yahoo.com>
2025-04-02 22:24:10 +0000

    Optimize README - handling edge cases

--- src/utils/ObjectPropertyCodeRetriever.ts
+++ src/utils/ObjectPropertyCodeRetriever.ts
@@ -1,11 +1,11 @@
 export class ObjectPropertyCodeRetriever {
     public get(object: any, propertyName: string): string {
-        const descriptor = Object.getOwnPropertyDescriptor(object, propertyName);
+        let descriptor = Object.getOwnPropertyDescriptor(object, propertyName);
         if (!descriptor) {
             // property is defined in prototype but has no descriptor (it comes from abstract class and was not override)
             return "";
         }
-        const accessorsCodes = [];
+        let accessorsCodes = [];
         if (descriptor.get) {
             accessorsCodes.push(descriptor.get.toString());
         }
